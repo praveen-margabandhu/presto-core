294736a7994973c279c329c5a67e42b3f9578245
praveen-margabandhu <prav8585@gmail.com>
2026-05-09 14:10:46 -0400

    Add package init file

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from parser.nfr_parser import NFRParser, NFRSpec, NFRValidationError
+from generator.k6_generator import K6Generator
+
+__version__ = "0.1.0"
+__all__ = ["NFRParser", "NFRSpec", "NFRValidationError", "K6Generator"]
